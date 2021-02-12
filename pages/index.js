9d566b86bb4708f62f2dc375a15c774e5d97724b
ChClaude <christ.tchambila@gmail.com>
2021-02-12 20:26:18 +0200

    Next JS updates

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,28 @@
 import Head from 'next/head';
+import ArticleList from '../components/ArticleList';
 
-
-export default function Home(articles) {
-  console.log(articles);
+export default function Home({ articles }) {
   return (
     <div>
       <Head>
         <title>About</title>
       </Head>
-     {articles.map(article => <h3>{article.title}</h3>)}
+      <ArticleList articles={articles} />
     </div>
   );
 }
 
 export const getStaticProps = async () => {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`);
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=6`
+  );
   const articles = await res.json();
+
+  console.log(articles);
 
   return {
     props: {
-      articles
-    }
-  }
+      articles,
+    },
+  };
 };
